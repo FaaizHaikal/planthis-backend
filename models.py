27a708547f2e3307a56b010ebcdca75b46197a64
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-28 19:59:21 +0700

    feat: add models

---
+++ models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class BiophysicalInput(BaseModel):
+  lat: float
+  lon: float
+
+class SoilData(BaseModel):
+  clay: float
+  sand: float
+  silt: float
+  ph: float
+  soc: float
+  types: List[str]
+
+class ClimateData(BaseModel):
+  temperature: float  # °C
+  rainfall: float     # mm/year
+
+class SpeciesMatchResult(BaseModel):
+  altitude: float
+  climate: ClimateData
+  soil: SoilData
+  matching_species: List[str]
